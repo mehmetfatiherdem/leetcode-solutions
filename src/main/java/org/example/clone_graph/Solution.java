a9b5de890319e079c1bdc54bb42dbbdd8eab3492
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-02-17 14:16:43 +0300

    made node class non-static in clone graph

--- src/main/java/org/example/clone_graph/Solution.java
+++ src/main/java/org/example/clone_graph/Solution.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    static class Node {
+    class Node {
         public int val;
         public List<Node> neighbors;
 
