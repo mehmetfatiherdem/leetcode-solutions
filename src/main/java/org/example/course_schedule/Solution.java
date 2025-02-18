50bff287398f3b88c4c87c9d4bbd3bc1fcafa84e
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-02-18 12:41:41 +0300

    207. Course Schedule

---
+++ src/main/java/org/example/course_schedule/Solution.java
@@ -0,0 +1,42 @@
+package org.example.course_schedule;
+
+import java.util.*;
+
+public class Solution {
+    public boolean dfs(int crs, Set<Integer> vs, Map<Integer, List<Integer>> hm) {
+        if(vs.contains(crs)) {
+            return false;
+        } else if(hm.get(crs).size() == 0) {
+            return true;
+        }
+        vs.add(crs);
+        for(int req: hm.get(crs)) {
+            if(!dfs(req, vs, hm)) return false;
+        }
+
+        vs.remove(crs);
+        hm.put(crs, new ArrayList<>());
+        return true;
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(prerequisites.length == 0) return true;
+        Map<Integer, List<Integer>> hm = new HashMap<>();
+
+        for(int i = 0; i<numCourses; i++) {
+            hm.put(i, new ArrayList<>());
+        }
+
+        for(int i = 0; i<prerequisites.length; i++) {
+            hm.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+
+        Set<Integer> vs = new HashSet<>();
+
+        for(int j = 0; j<numCourses; j++) {
+            if(!dfs(j, vs, hm)) return false;
+        }
+
+        return true;
+    }
+}
