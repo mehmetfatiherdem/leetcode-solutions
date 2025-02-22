178a64527c34434cc8c5cb920cf57e4f960a9eb7
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-02-22 12:20:46 +0300

    57. Insert Interval

---
+++ src/main/java/org/example/insert_interval/Solution.java
@@ -0,0 +1,31 @@
+package org.example.insert_interval;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0, n = intervals.length;
+        int ns = newInterval[0], ne = newInterval[1];
+
+        while (i < n && intervals[i][1] < ns) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        while (i < n && intervals[i][0] <= ne) {
+            ns = Math.min(ns, intervals[i][0]);
+            ne = Math.max(ne, intervals[i][1]);
+            i++;
+        }
+        result.add(new int[]{ns, ne});
+
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
