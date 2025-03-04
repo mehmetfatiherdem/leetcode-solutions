048b52a5030d870add1c7fb0ba3ae7f3ea50d1fe
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-04 05:45:33 +0300

    448. Find All Numbers Disappeared in an Array

---
+++ src/main/java/org/example/find_all_numbers_disappeared_in_an_array/Solution.java
@@ -0,0 +1,32 @@
+package org.example.find_all_numbers_disappeared_in_an_array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        int in = -1;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i<n; i++) {
+            if(nums[i] < 0) {
+                in = (nums[i]*(-1)) - 1;
+            } else {
+                in = nums[i]-1;
+            }
+
+            if(nums[in] > 0) {
+                nums[in] *=(-1);
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0){
+                list.add(i+1);
+            }
+
+        }
+
+        return list;
+    }
+}
