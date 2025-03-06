1fa84d20273e3e4cf6b3cfc843ba905cda2a1664
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-07 00:37:35 +0300

    5. Longest Palindromic Substring

---
+++ src/main/java/org/example/longest_palindromic_substring/Solution.java
@@ -0,0 +1,33 @@
+package org.example.longest_palindromic_substring;
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        String st = new String();
+        int slen = s.length();
+        int maxlen = 0;
+        for(int i = 0; i<slen; i++) {
+            int l = i, r = i;
+            while(l>=0 && r<slen && s.charAt(l) == s.charAt(r)) {
+                if(r-l+1 > maxlen) {
+                    st = s.substring(l, r+1);
+                    maxlen = r-l+1;
+                }
+                l--;
+                r++;
+            }
+
+            l = i;
+            r = i+1;
+            while(l>=0 && r<slen && s.charAt(l) == s.charAt(r)) {
+                if(r-l+1 > maxlen) {
+                    st = s.substring(l, r+1);
+                    maxlen = r-l+1;
+                }
+                l--;
+                r++;
+            }
+        }
+
+        return st;
+    }
+}
