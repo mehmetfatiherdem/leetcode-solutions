fb5341edf738d057e8d59268940d4e0bed2ddcfc
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-06 06:14:23 +0300

    49. Group Anagrams

---
+++ src/main/java/org/example/group_anagrams/Solution.java
@@ -0,0 +1,20 @@
+package org.example.group_anagrams;
+
+import java.util.*;
+
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> ans = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList<>());
+            }
+            ans.get(key).add(s);
+        }
+
+        return new ArrayList<>(ans.values());
+    }
+}
