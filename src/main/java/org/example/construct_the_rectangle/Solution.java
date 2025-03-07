439151e4fceae2e830511efd361fa1e4770e5e82
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-08 00:43:30 +0300

    492. Construct the Rectangle

---
+++ src/main/java/org/example/construct_the_rectangle/Solution.java
@@ -0,0 +1,16 @@
+package org.example.construct_the_rectangle;
+
+public class Solution {
+    public int[] constructRectangle(int area) {
+        int[] ar = new int[2];
+        int w = (int)Math.sqrt(area);
+        while(w>=2 && area%w != 0) {
+            w--;
+        }
+
+        ar[0] = area/w;
+        ar[1] = w;
+
+        return ar;
+    }
+}
