c49199b69f76c51c77975ab9018946a6cfcbb43c
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-09 00:34:05 +0300

    520. Detect Capital

---
+++ src/main/java/org/example/detect_capital/Solution.java
@@ -0,0 +1,23 @@
+package org.example.detect_capital;
+
+public class Solution {
+    public boolean detectCapitalUse(String word) {
+        boolean allCap = false;
+        boolean allLow = true;
+        boolean onlyFirstCap = false;
+        if(Character.isUpperCase(word.charAt(0))) {
+            onlyFirstCap = true;
+            allLow = false;
+            allCap = true;
+        }
+        for(int i = 1; i<word.length(); i++) {
+            if(Character.isUpperCase(word.charAt(i))) {
+                onlyFirstCap = false;
+                allLow = false;
+            } else {
+                allCap = false;
+            }
+        }
+        return allCap || allLow || onlyFirstCap;
+    }
+}
