8afeb741c7d9b4be4f3e8723b8e663b67bb54b55
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-10 00:55:01 +0300

    297. Serialize and Deserialize Binary Tree

---
+++ src/main/java/org/example/serialize_and_deserialize_binary_tree/Solution.java
@@ -0,0 +1,49 @@
+package org.example.serialize_and_deserialize_binary_tree;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+
+    class TreeNode {
+        int val;
+        TreeNode right;
+        TreeNode left;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serializer(root, sb);
+        return sb.toString();
+    }
+
+    public void serializer(TreeNode node, StringBuilder sb) {
+        if(node == null) {
+            sb.append("#,");
+            return;
+        }
+        sb.append(node.val).append(",");
+        serializer(node.left, sb);
+        serializer(node.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+        return deserializer(queue);
+    }
+
+    public TreeNode deserializer(Queue<String> queue) {
+        String str = queue.poll();
+        if(str.equals("#")) return null;
+        TreeNode node = new TreeNode(Integer.valueOf(str));
+        node.left = deserializer(queue);
+        node.right = deserializer(queue);
+        return node;
+    }
+}
