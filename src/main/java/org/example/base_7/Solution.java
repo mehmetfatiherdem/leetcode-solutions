ea8ed2c6d1d3356857385ed8f4b631475d39ba64
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-13 00:50:35 +0300

    504. Base 7

---
+++ src/main/java/org/example/base_7/Solution.java
@@ -0,0 +1,21 @@
+package org.example.base_7;
+
+public class Solution {
+    public String convertToBase7(int num) {
+        if(num == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        int t = num;
+        int r = 1;
+        if(num<0) t = t*(-1);
+        while(t > 0) {
+            r = t%7;
+            t = t/7;
+            sb.append(r);
+
+        }
+
+        if(num<0) sb.append("-");
+
+        return sb.reverse().toString();
+    }
+}
