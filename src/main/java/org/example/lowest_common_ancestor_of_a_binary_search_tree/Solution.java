ae932d73b621dcae4d3cf1ad56abb41ce943d212
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-14 01:07:44 +0300

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ src/main/java/org/example/lowest_common_ancestor_of_a_binary_search_tree/Solution.java
@@ -0,0 +1,14 @@
+package org.example.lowest_common_ancestor_of_a_binary_search_tree;
+
+import org.example.data_structures.TreeNode;
+
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if((p.val <= root.val && q.val >= root.val) || (q.val <= root.val && p.val >= root.val)) {
+            return root;
+        } else if(p.val<root.val && q.val<root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return lowestCommonAncestor(root.right, p, q);
+    }
+}
