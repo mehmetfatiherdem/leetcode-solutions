ad08f98d133000a07a9a332a9503ef3658aafd0d
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-14 01:05:12 +0300

    561. Array Partition

---
+++ src/main/java/org/example/array_partition/Solution.java
@@ -0,0 +1,14 @@
+package org.example.array_partition;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for(int i = 0; i<nums.length; i+=2) {
+            sum+=nums[i];
+        }
+        return sum;
+    }
+}
