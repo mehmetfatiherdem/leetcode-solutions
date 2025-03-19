e33902b554f1f1745c548065eda7c4c91a8c03fc
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-20 02:13:36 +0300

    637. Average of Levels in Binary Tree

---
+++ src/main/java/org/example/average_of_levels_in_binary_tree/Solution.java
@@ -0,0 +1,40 @@
+package org.example.average_of_levels_in_binary_tree;
+
+import org.example.data_structures.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> li = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode nl = root.left;
+        q.add(root);
+        double sum = root.val;
+        int lnum = 1;
+        while(!q.isEmpty()) {
+            root = q.poll();
+            if(root != nl) {
+                sum+=root.val;
+                lnum++;
+            } else {
+                li.add(sum/lnum);
+                sum = root.val;
+                lnum = 1;
+                nl = null;
+            }
+            if(root.left != null) q.add(root.left);
+            if(root.right != null) q.add(root.right);
+            if(nl == null) {
+                nl = root.left == null ? root.right:root.left;
+            }
+
+        }
+        li.add(sum/lnum);
+
+        return li;
+    }
+}
