8e615123415062de7b7b7f03a0597ddccf40bbe8
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-24 01:53:24 +0300

    Meeting Rooms

---
+++ src/main/java/org/example/meeting_rooms/Solution.java
@@ -0,0 +1,28 @@
+package org.example.meeting_rooms;
+
+import java.util.List;
+import java.util.Comparator;
+import java.util.Collections;
+
+public class Solution {
+
+    public class Interval {
+      public int start, end;
+      public Interval(int start, int end) {
+          this.start = start;
+          this.end = end;
+      }
+    }
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if(intervals.size() == 0) return true;
+        Comparator<Interval> intcom = Comparator.comparingInt((Interval i) -> i.start);
+        Collections.sort(intervals, intcom);
+        Interval prev = intervals.get(0);
+        for(int i = 1; i<intervals.size(); i++) {
+            if(intervals.get(i).start < prev.end) return false;
+            prev = intervals.get(i);
+        }
+
+        return true;
+    }
+}
