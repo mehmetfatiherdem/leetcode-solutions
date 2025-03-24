c3d54c56db13eab7fab8a7a4564a71cad580add1
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-25 02:38:49 +0300

    Meeting Rooms II

---
+++ src/main/java/org/example/meeting_rooms_ii/Solution.java
@@ -0,0 +1,32 @@
+package org.example.meeting_rooms_ii;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Solution {
+
+    public class Interval {
+        public int start, end;
+        public Interval(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+    public int minMeetingRooms(List<Interval> intervals) {
+
+        Collections.sort(intervals,
+                Comparator.comparingInt((Interval i1) -> i1.start));
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (Interval interval : intervals) {
+            if (!minHeap.isEmpty() && minHeap.peek()
+                    <= interval.start) {
+                minHeap.poll();
+            }
+            minHeap.offer(interval.end);
+        }
+        return minHeap.size();
+    }
+}
