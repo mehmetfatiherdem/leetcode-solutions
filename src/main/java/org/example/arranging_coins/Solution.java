a7e9d654361d7f69dca9358db6694705ddc47980
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-26 01:35:30 +0300

    441. Arranging Coins

---
+++ src/main/java/org/example/arranging_coins/Solution.java
@@ -0,0 +1,24 @@
+package org.example.arranging_coins;
+
+public class Solution {
+    public int arrangeCoins(int n) {
+        if(n == 1) return 1;
+
+        long sum = 0;
+        long m = 0;
+        long l = 0, r = n;
+        while(l<=r) {
+            m = l+(r-l)/2;
+            sum = m*(m+1)/2;
+            if(sum > n) {
+                r = m-1;
+            } else if(sum<n) {
+                l = m+1;
+            } else {
+                return (int)m;
+            }
+        }
+
+        return (int)r;
+    }
+}
