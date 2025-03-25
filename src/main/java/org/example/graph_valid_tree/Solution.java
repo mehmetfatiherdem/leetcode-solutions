a42b76c8b44a8b3a7943c6a90bcb795f8835f726
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-26 01:39:59 +0300

    Graph Valid Tree

---
+++ src/main/java/org/example/graph_valid_tree/Solution.java
@@ -0,0 +1,43 @@
+package org.example.graph_valid_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        if (edges.length != n - 1) return false;
+        if(edges.length == 0) return true;
+        boolean[] visited = new boolean[n];
+
+        List<List<Integer>> adj = new ArrayList<>(edges.length);
+        for(int i = 0; i<n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0; i<edges.length; i++) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+
+        if(!dfs(0,0,adj,visited)) return false;
+
+        for (boolean visit : visited) {
+            if (!visit) return false;
+        }
+
+        return true;
+    }
+
+    boolean dfs(int prev, int num,  List<List<Integer>> adj, boolean[] visited) {
+
+        visited[num] = true;
+
+        for(int n: adj.get(num)) {
+            if(prev == n) continue;
+            if(visited[n]) return false;
+            if(!dfs(num,n, adj, visited)) return false;
+        }
+
+        return true;
+    }
+}
+
