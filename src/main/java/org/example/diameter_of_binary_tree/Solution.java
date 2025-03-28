b15f6f021a0a688f073a905c1f65a33f550ed699
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-29 00:57:33 +0300

    543. Diameter of Binary Tree

---
+++ src/main/java/org/example/diameter_of_binary_tree/Solution.java
@@ -0,0 +1,21 @@
+package org.example.diameter_of_binary_tree;
+
+import org.example.data_structures.TreeNode;
+
+public class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null) return -1;
+        int[]res = {0};
+        int lh = d(root, res);
+
+        return res[0];
+    }
+    int d(TreeNode root, int[]res) {
+        if(root == null) return 0;
+
+        int lh = d(root.left, res);
+        int rh = d(root.right, res);
+        res[0] = Math.max(res[0],lh+rh);
+        return Math.max(lh,rh)+1;
+    }
+}
