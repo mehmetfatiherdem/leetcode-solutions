c4253c1c3d346052a4ad41c4d9b8f23f9659ab1a
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-29 00:56:22 +0300

    563. Binary Tree Tilt

---
+++ src/main/java/org/example/binary_tree_tilt/Solution.java
@@ -0,0 +1,26 @@
+package org.example.binary_tree_tilt;
+
+import org.example.data_structures.TreeNode;
+
+public class Solution {
+    int res = 0;
+    public int findTilt(TreeNode root) {
+        if(root == null) return 0;
+
+        f(root);
+
+        return res;
+    }
+
+    int f(TreeNode root) {
+        if(root == null) return 0;
+
+        int lsum = f(root.left);
+        int rsum = f(root.right);
+
+        res += Math.abs(lsum - rsum);
+
+        return root.val+lsum+rsum;
+    }
+
+}
