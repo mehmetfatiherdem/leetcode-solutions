9d0d3218a3f2a5b4ed91139d8ed415281fcc3f5b
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-03-31 10:50:09 +0300

    728. Self Dividing Numbers

---
+++ src/main/java/org/example/self_dividing_numbers/Solution.java
@@ -0,0 +1,30 @@
+package org.example.self_dividing_numbers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> li = new ArrayList<>();
+
+        int msd = 0;
+        while(left<=right) {
+            int tl = left;
+            if(dv(left)) li.add(left);
+            left++;
+        }
+
+        return li;
+    }
+
+    boolean dv(int num) {
+        int t = num;
+        while(t>0) {
+            if(t%10 == 0 || num%(t%10)!=0) return false;
+            t = t/10;
+
+        }
+
+        return true;
+    }
+}
