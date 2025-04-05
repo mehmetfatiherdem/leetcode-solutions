65a772a1e2894bf856a632108c014f1129b2ab18
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-04-06 00:02:40 +0300

    Two Sum II - Input Array Is Sorted

---
+++ src/main/java/org/example/two_sum_ii_input_array_is_sorted/Solution.java
@@ -0,0 +1,19 @@
+package org.example.two_sum_ii_input_array_is_sorted;
+
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l =0 ,r =numbers.length-1;
+
+        while(l<r){
+            int total = numbers[l] +numbers[r];
+
+            if(total == target){
+                return new int[]{l+1,r+1};
+            }
+            else if(total > target) r--;
+
+            else l++;
+        }
+        return new int[]{-1,-1};
+    }
+}
