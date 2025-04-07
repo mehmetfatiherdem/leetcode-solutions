d75e1f2cb0f5ffb41c1fe5c04ab7faf40fd0afc4
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-04-07 10:50:27 +0300

    42. Trapping Rain Water

---
+++ src/main/java/org/example/trapping_rain_water/Solution.java
@@ -0,0 +1,21 @@
+package org.example.trapping_rain_water;
+
+public class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int lp = 0, rp = n-1;
+        int lmax = height[lp], rmax = height[rp];
+        int sum = 0;
+        while(lp<rp) {
+            if(lmax>rmax) {
+                rmax = Math.max(rmax, height[--rp]);
+                sum+=rmax-height[rp];
+            } else {
+                lmax = Math.max(lmax, height[++lp]);
+                sum+=lmax-height[lp];
+            }
+        }
+
+        return sum;
+    }
+}
