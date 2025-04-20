c1b21e7dbaa985a62aaf8dffd6d184128616d369
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-04-20 09:42:53 +0300

    739. Daily Temperatures

---
+++ src/main/java/org/example/daily_temperatures/Solution.java
@@ -0,0 +1,20 @@
+package org.example.daily_temperatures;
+
+import java.util.Stack;
+
+public class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] days = new int[temperatures.length];
+        days[temperatures.length-1] = 0;
+        for(int i = 0; i<temperatures.length; i++) {
+            while(!stack.empty() && temperatures[stack.peek()]<temperatures[i]) {
+                int ind = stack.pop();
+                days[ind] = i-ind;
+            }
+            stack.push(i);
+        }
+
+        return days;
+    }
+}
