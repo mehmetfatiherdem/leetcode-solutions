1f46e1be6101fc04cb811821c07c2b6d90b31042
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-05-07 10:22:15 +0300

    1934. Confirmation Rate

---
+++ src/main/java/org/example/confirmation_rate/solution.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+select s.user_id,
+round( (sum(case when c.action = 'confirmed' then 1 else 0 end)/count(*)::numeric),2) as confirmation_rate
+from Signups s
+left join Confirmations c
+on s.user_id = c.user_id
+group by s.user_id
