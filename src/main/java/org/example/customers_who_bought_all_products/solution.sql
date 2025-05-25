c2d07d21962e4a50cf0f8ce51c266ee467c650fe
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-05-25 20:37:31 +0300

    1045. Customers Who Bought All Products

---
+++ src/main/java/org/example/customers_who_bought_all_products/solution.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+select c.customer_id
+from Customer c
+group by c.customer_id
+having count(distinct(c.product_key)) = (select count(product_key) from Product)
