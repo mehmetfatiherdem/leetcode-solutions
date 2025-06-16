4b7ce95dda77bc64122a5486b18eadf0457cb18c
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-06-16 10:18:49 +0300

    853. Car Fleet

---
+++ src/main/java/org/example/car_fleet/Solution.java
@@ -0,0 +1,37 @@
+package org.example.car_fleet;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+
+        double[][] cars = new double[n][2];
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+
+        int cn = 0;
+
+        int topArrival = 0;
+
+        for (int i = 0; i<cars.length; i++) {
+
+            double arrivalTime = (double)(target - cars[i][0]) / cars[i][1];
+            cars[i][1] = arrivalTime;
+            //topArrival = i;
+            cn++;
+
+            if (cn >= 2 && cars[topArrival][1] >= cars[i][1]) {
+                cn--;
+            } else if (cn >= 2 && cars[topArrival][1] < cars[i][1]) {
+                topArrival = i;
+            }
+        }
+
+        return cn;
+    }
+}
