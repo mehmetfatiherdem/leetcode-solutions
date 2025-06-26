e86b43d26095030a433a82219d9b4302e637e7cc
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-06-26 10:19:28 +0300

    78. Subsets

---
+++ src/main/java/org/example/subsets/Solution.java
@@ -0,0 +1,24 @@
+package org.example.subsets;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> li = new ArrayList<>();
+        subsets(nums, li, new ArrayList<Integer>(), 0);
+        return li;
+    }
+
+    public void subsets(int[] nums, List<List<Integer>> li, List<Integer> ss, int i) {
+        if(i == nums.length) {
+            li.add(new ArrayList<>(ss));
+            return;
+        }
+
+        ss.add(nums[i]);
+        subsets(nums, li, ss, i+1);
+        ss.remove(ss.size()-1);
+        subsets(nums, li, ss, i+1);
+    }
+}
