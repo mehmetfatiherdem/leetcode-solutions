4efd0a88e662c2c3a5616296f6477450ceb3220e
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-06-28 09:33:48 +0300

    199. Binary Tree Right Side View

---
+++ src/main/java/org/example/binary_tree_right_side_view/Solution.java
@@ -0,0 +1,34 @@
+package org.example.binary_tree_right_side_view;
+
+import org.example.data_structures.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root == null) return new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
+
+        Queue<TreeNode> qu = new LinkedList<>();
+        qu.add(root);
+        qu.add(null);
+
+        while(!qu.isEmpty()) {
+            root = qu.poll();
+
+            if(root.left != null) qu.add(root.left);
+            if(root.right != null) qu.add(root.right);
+
+            if(qu.peek() == null) {
+                list.add(root.val);
+                qu.poll();
+                if(!qu.isEmpty()) qu.add(null);
+            }
+        }
+
+        return list;
+    }
+}
