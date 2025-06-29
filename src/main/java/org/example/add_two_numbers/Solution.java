3a9fdf320d6de931a2f9f4f40afc42e406b7b01b
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-06-29 09:28:14 +0300

    2. Add Two Numbers

---
+++ src/main/java/org/example/add_two_numbers/Solution.java
@@ -0,0 +1,57 @@
+package org.example.add_two_numbers;
+
+import org.example.data_structures.ListNode;
+
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode h = new ListNode(0);
+        ListNode th = h;
+        int cin = 0;
+        while (l1.next != null && l2.next != null) {
+            int s = l1.val + l2.val + cin;
+            int d = s % 10;
+            th.val = d;
+            cin = s / 10;
+            l1 = l1.next;
+            l2 = l2.next;
+            ListNode nn = new ListNode(0);
+            th.next = nn;
+            th = nn;
+        }
+
+        while (l1 != null && l2 != null) {
+            int s = l1.val + l2.val + cin;
+            int d = s % 10;
+            th.val = d;
+            cin = s / 10;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while (l1 != null) {
+            th.next = new ListNode(0);
+            th = th.next;
+            int s = l1.val + cin;
+            int d = s % 10;
+            th.val = d;
+            cin = s / 10;
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            th.next = new ListNode(0);
+            th = th.next;
+            int s = l2.val + cin;
+            int d = s % 10;
+            th.val = d;
+            cin = s / 10;
+            l2 = l2.next;
+        }
+
+        if (cin > 0) {
+            th.next = new ListNode(cin);
+        }
+
+        return h;
+    }
+}
--- src/main/java/org/example/data_structures/ListNode.java
+++ src/main/java/org/example/data_structures/ListNode.java
@@ -3,5 +3,5 @@ package org.example.data_structures;
 public class ListNode {
     public int val;
     public ListNode next;
-    ListNode(int x) { val = x; }
+    public ListNode(int x) { val = x; }
 }
