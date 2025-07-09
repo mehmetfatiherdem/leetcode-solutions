4769a055fef6f83940ba15db4750cb9b57b94fca
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-07-09 10:06:44 +0300

    9. Palindrome Number

---
+++ src/main/java/org/example/palindrome_number/Solution.java
@@ -0,0 +1,22 @@
+package org.example.palindrome_number;
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        if(x!=0 && (x%10 == 0)){
+            return false;
+        }
+        int pal = 0;
+        int num = x;
+        while(num>0){
+            pal = (pal*10) + (num%10);
+            num = num/10;
+        }
+        if(pal != x){
+            return false;
+        }
+        return true;
+    }
+}
