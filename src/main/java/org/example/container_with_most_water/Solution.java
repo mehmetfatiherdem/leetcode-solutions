cc1379e14b212a9be59dd5351f1fd2e470925f3d
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-07-12 09:29:05 +0300

    11. Container With Most Water

---
+++ src/main/java/org/example/container_with_most_water/Solution.java
@@ -0,0 +1,24 @@
+package org.example.container_with_most_water;
+
+public class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int i = 0, j = n-1;
+        int maxArea = 0;
+        int w, h;
+        while(j > i) {
+
+            w = j - i;
+            if(height[i] > height[j]) {
+                h = height[j--];
+            } else {
+                h = height[i++];
+            }
+
+            int area = w * h;
+            maxArea = Math.max(area, maxArea);
+        }
+
+        return maxArea;
+    }
+}
