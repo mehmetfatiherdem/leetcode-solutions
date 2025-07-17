9284498e1138eec8e778174ad62440344090c608
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-07-17 09:42:08 +0300

    refactor decremenentation

--- src/main/java/org/example/add_strings/Solution.java
+++ src/main/java/org/example/add_strings/Solution.java
@@ -9,11 +9,9 @@ public class Solution {
         while(i >= 0 || j >= 0) {
             int n1 = 0, n2 = 0;
             if(i>=0) {
-                n1 = num1.charAt(i) - '0';
-                i--;
+                n1 = num1.charAt(i--) - '0';
             } if(j>=0) {
-                n2 = num2.charAt(j) - '0';
-                j--;
+                n2 = num2.charAt(j--) - '0';
             }
 
             int sum = n1+n2+carry;
