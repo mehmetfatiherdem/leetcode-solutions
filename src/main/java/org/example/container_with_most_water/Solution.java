987c94a05dbf3743f2f29ed443e8fdceceb65a1f
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-07-25 08:57:58 +0300

    newline a var

--- src/main/java/org/example/container_with_most_water/Solution.java
+++ src/main/java/org/example/container_with_most_water/Solution.java
@@ -3,7 +3,8 @@ package org.example.container_with_most_water;
 public class Solution {
     public int maxArea(int[] height) {
         int n = height.length;
-        int i = 0, j = n-1;
+        int i = 0;
+        int j = n-1;
         int maxArea = 0;
         int w, h;
         while(j > i) {
