d9cb927bd716a147cb3fe2d113d85d601895f880
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-07-28 09:46:16 +0300

    switch to foreach

--- src/main/java/org/example/contains_duplicate/Solution.java
+++ src/main/java/org/example/contains_duplicate/Solution.java
@@ -8,10 +8,10 @@ public class Solution {
 
         Set<Integer> s = new HashSet<>();
 
-        for(int i = 0; i<nums.length; i++){
-            if(s.contains(nums[i])) return true;
+        for (int num : nums) {
+            if (s.contains(num)) return true;
 
-            s.add(nums[i]);
+            s.add(num);
         }
         return false;
     }
