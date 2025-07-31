0a9f82862d76eaf9f5f8f2d6b1d542585f9d51fc
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-07-31 09:59:55 +0300

    new line a variable

--- src/main/java/org/example/best_time_to_buy_and_sell_stock/Solution.java
+++ src/main/java/org/example/best_time_to_buy_and_sell_stock/Solution.java
@@ -2,7 +2,8 @@ package org.example.best_time_to_buy_and_sell_stock;
 
 public class Solution {
     public int maxProfit(int[] prices) {
-        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
         for (int price : prices) {
             min = Math.min(min, price);
