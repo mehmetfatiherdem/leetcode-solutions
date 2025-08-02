c477d3a4bedce51069c3ca0e6f25ad0e2e00c1ab
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-08-02 16:15:14 +0300

    declare length

--- src/main/java/org/example/daily_temperatures/Solution.java
+++ src/main/java/org/example/daily_temperatures/Solution.java
@@ -5,10 +5,11 @@ import java.util.Stack;
 public class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         Stack<Integer> stack = new Stack<>();
-        int[] days = new int[temperatures.length];
-        days[temperatures.length-1] = 0;
+        int tlen = temperatures.length;
+        int[] days = new int[tlen];
+        days[tlen-1] = 0;
         int i = 0;
-        for(; i<temperatures.length; i++) {
+        for(; i<tlen; i++) {
             while(!stack.empty() && temperatures[stack.peek()]<temperatures[i]) {
                 int ind = stack.pop();
                 days[ind] = i-ind;
