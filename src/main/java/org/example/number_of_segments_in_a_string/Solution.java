1a02df3f052b93abc33e8eb3127c8aad5f504a23
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-09-12 10:23:05 +0300

    remove unnecessary null check

--- src/main/java/org/example/number_of_segments_in_a_string/Solution.java
+++ src/main/java/org/example/number_of_segments_in_a_string/Solution.java
@@ -2,7 +2,7 @@ package org.example.number_of_segments_in_a_string;
 
 public class Solution {
     public int countSegments(String s) {
-        if(s.isEmpty() || s==null) return 0;
+        if(s.isEmpty()) return 0;
         int cnt = 0;
         int i = 0;
         while(i<s.length()) {
