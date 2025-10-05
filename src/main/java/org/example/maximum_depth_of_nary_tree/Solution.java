b96c0a6bfad5ffaa950ab647c5d2fa0fe13106e8
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-10-05 10:22:42 +0300

    use isempty for size check

--- src/main/java/org/example/maximum_depth_of_nary_tree/Solution.java
+++ src/main/java/org/example/maximum_depth_of_nary_tree/Solution.java
@@ -20,7 +20,7 @@ public class Solution {
     }
     public int maxDepth(Node root) {
         if(root == null) return 0;
-        if(root.children.size() == 0) return 1;
+        if(root.children.isEmpty()) return 1;
         int max = Integer.MIN_VALUE;
         for(Node n: root.children) {
             int len = maxDepth(n);
