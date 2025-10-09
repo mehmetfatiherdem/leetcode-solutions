81111a68ab9460b407ff59ad2b693c7a6f132c75
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-10-09 10:19:14 +0300

    import narytree instead of using node

--- src/main/java/org/example/maximum_depth_of_nary_tree/Solution.java
+++ src/main/java/org/example/maximum_depth_of_nary_tree/Solution.java
@@ -1,18 +1,13 @@
 package org.example.maximum_depth_of_nary_tree;
 
-import java.util.List;
+import org.example.data_structures.NaryTree;
 
 public class Solution {
-    class Node {
-        public int val;
-        public List<Node> children;
-
-    }
-    public int maxDepth(Node root) {
+    public int maxDepth(NaryTree root) {
         if(root == null) return 0;
         if(root.children.isEmpty()) return 1;
         int max = Integer.MIN_VALUE;
-        for(Node n: root.children) {
+        for(NaryTree n: root.children) {
             int len = maxDepth(n);
             max = Math.max(max, len+1);
         }
