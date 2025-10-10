ff812da222786f059382f198b822e1316fc87662
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-10-10 10:26:38 +0300

    simplify conditional

--- src/main/java/org/example/lowest_common_ancestor_of_a_binary_search_tree/Solution.java
+++ src/main/java/org/example/lowest_common_ancestor_of_a_binary_search_tree/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if((p.val <= root.val && q.val >= root.val) || (q.val <= root.val && p.val >= root.val)) {
             return root;
-        } else if(p.val<root.val && q.val<root.val) {
+        } else if(p.val<root.val) {
             return lowestCommonAncestor(root.left, p, q);
         }
         return lowestCommonAncestor(root.right, p, q);
