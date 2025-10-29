157b7f8487f8a7066eb124d3a3ebaefce8a6f834
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-10-29 11:41:35 +0300

    newline i

--- src/main/java/org/example/is_subsequence/Solution.java
+++ src/main/java/org/example/is_subsequence/Solution.java
@@ -2,7 +2,8 @@ package org.example.is_subsequence;
 
 public class Solution {
     public boolean isSubsequence(String s, String t) {
-        int slen = s.length(), tlen = t.length(), i = 0;
+        int slen = s.length(), tlen = t.length();
+        int i = 0;
         int j = 0;
         while(i<slen && j<tlen) {
             if(s.charAt(i) != t.charAt(j)) {
