f912648f71267409de31768c2675d95486ae099d
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-11-14 11:27:03 +0300

    remove brackets in cond

--- src/main/java/org/example/find_all_numbers_disappeared_in_an_array/Solution.java
+++ src/main/java/org/example/find_all_numbers_disappeared_in_an_array/Solution.java
@@ -14,15 +14,11 @@ public class Solution {
                 in = nums[i]-1;
             }
 
-            if(nums[in] > 0) {
-                nums[in] *=(-1);
-            }
+            if(nums[in] > 0) nums[in] *=(-1);
         }
 
         for(int i = 0; i < nums.length; i++){
             if(nums[i] > 0) list.add(i+1);
-
-
         }
 
         return list;
