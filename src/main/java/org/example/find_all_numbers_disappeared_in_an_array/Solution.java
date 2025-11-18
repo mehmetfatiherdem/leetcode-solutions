7ffb195527d6c831521e920642c8356a1448bab0
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-11-18 10:48:37 +0300

    defer initializing list

--- src/main/java/org/example/find_all_numbers_disappeared_in_an_array/Solution.java
+++ src/main/java/org/example/find_all_numbers_disappeared_in_an_array/Solution.java
@@ -6,13 +6,14 @@ import java.util.List;
 public class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         int in;
-        List<Integer> list = new ArrayList<>();
+
         for(int i = 0; i<nums.length; i++) {
             if(nums[i] < 0) in = (nums[i]*(-1)) - 1;
             else in = nums[i]-1;
 
             if(nums[in] > 0) nums[in] *=(-1);
         }
+        List<Integer> list = new ArrayList<>();
 
         for(int i = 0; i < nums.length; i++) if(nums[i] > 0) list.add(i+1);
 
