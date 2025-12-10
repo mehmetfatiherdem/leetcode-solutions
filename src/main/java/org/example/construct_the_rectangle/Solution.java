c0a2be63dbc6e1614ebf97d7c0fc472e73928c0a
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-12-10 06:31:29 -0600

    inline while

--- src/main/java/org/example/construct_the_rectangle/Solution.java
+++ src/main/java/org/example/construct_the_rectangle/Solution.java
@@ -4,9 +4,8 @@ public class Solution {
     public int[] constructRectangle(int area) {
         int[] ar = new int[2];
         int w = (int)Math.sqrt(area);
-        while(w>=2 && area%w != 0) {
-            w--;
-        }
+        while(w>=2 && area%w != 0) w--;
+
 
         ar[0] = area/w;
         ar[1] = w;
