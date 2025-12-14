cac8454d5b4012b9ae32faf021078a2433319da7
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2025-12-14 07:06:03 -0600

    use for each instead of for

--- src/main/java/org/example/combination_sum_IV/Solution.java
+++ src/main/java/org/example/combination_sum_IV/Solution.java
@@ -6,10 +6,9 @@ public class Solution {
         dp[0] = 1;
 
         for(int i = 1; i <= target; i++) {
-            for(int j = 0; j < nums.length; j++) {
-                int num = nums[j];
-                if(i - num >= 0) {
-                    dp[i] += dp[i-num];
+            for (int num : nums) {
+                if (i - num >= 0) {
+                    dp[i] += dp[i - num];
                 }
             }
         }
