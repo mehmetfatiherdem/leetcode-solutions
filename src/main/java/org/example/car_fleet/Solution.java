193afe0b178880b8dabd2b1da72f6f5333ba5dc3
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-01-09 12:13:59 -0600

    delete unnecessary empty line

--- src/main/java/org/example/car_fleet/Solution.java
+++ src/main/java/org/example/car_fleet/Solution.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
         int n = position.length;
-
         double[][] cars = new double[n][2];
         for (int i = 0; i < n; i++) {
             cars[i][0] = position[i];
