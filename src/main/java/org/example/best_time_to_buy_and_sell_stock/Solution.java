c466d7d2b9a3e51417d68b19fb2e0130b926bf5f
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-01-28 07:50:15 -0600

    remove mt

--- src/main/java/org/example/best_time_to_buy_and_sell_stock/Solution.java
+++ src/main/java/org/example/best_time_to_buy_and_sell_stock/Solution.java
@@ -4,7 +4,6 @@ public class Solution {
     public int maxProfit(int[] prices) {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-
         for (int price : prices) {
             min = Math.min(min, price);
             max = Math.max(max, price - min);
