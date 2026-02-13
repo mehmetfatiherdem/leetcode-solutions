e1933afb41001784325657cd90dfe8aed08acbbd
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-02-13 00:20:46 -0600

    ınline condıtıon

--- src/main/java/org/example/two_sum/Solution.java
+++ src/main/java/org/example/two_sum/Solution.java
@@ -8,9 +8,8 @@ public class Solution {
         Map<Integer, Integer> numToIndex = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if (numToIndex.containsKey(complement)) {
-                return new int[]{numToIndex.get(complement), i};
-            }
+            if (numToIndex.containsKey(complement)) return new int[]{numToIndex.get(complement), i};
+
             numToIndex.put(nums[i], i);
         }
         return new int[0];
