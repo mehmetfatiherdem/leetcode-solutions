f6315de7ce45dff27cb1a6e9e9640ba43ae7ae34
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-02-22 02:02:51 -0600

    inline condition

--- src/main/java/org/example/add_strings/Solution.java
+++ src/main/java/org/example/add_strings/Solution.java
@@ -8,9 +8,8 @@ public class Solution {
 
         while(i >= 0 || j >= 0) {
             int n1 = 0, n2 = 0;
-            if(i>=0) {
-                n1 = num1.charAt(i--) - '0';
-            } if(j>=0) n2 = num2.charAt(j--) - '0';
+            if(i>=0) n1 = num1.charAt(i--) - '0';
+            if(j>=0) n2 = num2.charAt(j--) - '0';
 
             int sum = n1+n2+carry;
             if(sum >= 10) carry = 1;
