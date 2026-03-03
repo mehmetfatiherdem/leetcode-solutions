2c6903af782eacfb8e2c820c99e179cbb95a4494
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-03-03 01:35:22 -0600

    inline if

--- src/main/java/org/example/valid_perfect_square/Solution.java
+++ src/main/java/org/example/valid_perfect_square/Solution.java
@@ -6,9 +6,8 @@ public class Solution {
 
         while(l<=r) {
             long mid = l + (r-l)/2;
-            if(mid*mid > num) {
-                r = mid-1;
-            } else if(mid*mid < num) l = mid+1;
+            if(mid*mid > num) r = mid-1;
+            else if(mid*mid < num) l = mid+1;
             else return true;
         }
 
