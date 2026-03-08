07d07220b634f8276cd4859569dbfb043f42989b
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-03-08 03:52:13 -0500

    inline if

--- src/main/java/org/example/valid_parentheses/Solution.java
+++ src/main/java/org/example/valid_parentheses/Solution.java
@@ -10,9 +10,8 @@ public class Solution {
 
         Stack <Character> para = new Stack<>();
         for(char c : s.toCharArray()){
-            if(c == '(' || c == '{' || c == '['){
-                para.push(c);
-            }else if( c == ')' && !para.isEmpty() && para.peek() == '(' ) para.pop();
+            if(c == '(' || c == '{' || c == '[') para.push(c);
+            else if( c == ')' && !para.isEmpty() && para.peek() == '(' ) para.pop();
             else if( c == '}' && !para.isEmpty() && para.peek() == '{' ) para.pop();
             else if( c == ']' && !para.isEmpty() &&  para.peek() == '[' ) para.pop();
             else return false;
