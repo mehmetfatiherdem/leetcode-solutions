4e7a9322565900bdf7fd330a5a3fa18421919899
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-03-13 02:53:07 -0500

    remove loop brackets

--- src/main/java/org/example/unique_paths/Solution.java
+++ src/main/java/org/example/unique_paths/Solution.java
@@ -6,11 +6,11 @@ public class Solution {
         for(int y = 0; y < m; y++) dp[y][0] = 1;
         for(int x = 0; x < n; x++) dp[0][x] = 1;
 
-        for(int i = 1; i<m; i++) {
-            for(int j = 1; j<n; j++) {
+        for(int i = 1; i<m; i++)
+            for(int j = 1; j<n; j++)
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
+
+
 
         return dp[m-1][n-1];
     }
