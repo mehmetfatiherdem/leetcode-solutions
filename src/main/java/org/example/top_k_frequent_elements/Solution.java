9691bb3be8bd5685badeae49286fcb3a05c0dd74
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-03-21 14:00:45 -0500

    inline loop

--- src/main/java/org/example/top_k_frequent_elements/Solution.java
+++ src/main/java/org/example/top_k_frequent_elements/Solution.java
@@ -8,9 +8,8 @@ public class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> hm = new HashMap<>();
 
-        for(int n: nums) {
-            hm.put(n, hm.getOrDefault(n, 0)+1);
-        }
+        for(int n: nums) hm.put(n, hm.getOrDefault(n, 0)+1);
+
 
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> hm.get(b)-hm.get(a));
         pq.addAll(hm.keySet());
