fbfd17abfc3c668db5b178d51e13e0be7d947295
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-03-24 00:40:42 -0500

    inline if

--- src/main/java/org/example/to_lower_case/Solution.java
+++ src/main/java/org/example/to_lower_case/Solution.java
@@ -4,9 +4,8 @@ public class Solution {
     public String toLowerCase(String s) {
         char[] car = s.toCharArray();
         for(int i = 0; i<car.length; i++) {
-            if(car[i]>='A' && car[i]<='Z') {
-                car[i] = (char)('a'+(car[i]-'A'));
-            }
+            if(car[i]>='A' && car[i]<='Z') car[i] = (char)('a'+(car[i]-'A'));
+
         }
         return new String(car);
     }
