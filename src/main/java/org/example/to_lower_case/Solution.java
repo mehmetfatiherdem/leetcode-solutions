13b908e314959b468b80465789d1af7a6a899d5e
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-03-26 12:47:50 -0500

    inline loop

--- src/main/java/org/example/to_lower_case/Solution.java
+++ src/main/java/org/example/to_lower_case/Solution.java
@@ -3,9 +3,7 @@ package org.example.to_lower_case;
 public class Solution {
     public String toLowerCase(String s) {
         char[] car = s.toCharArray();
-        for(int i = 0; i<car.length; i++) {
-            if(car[i]>='A' && car[i]<='Z') car[i] = (char)('a'+(car[i]-'A'));
-        }
+        for(int i = 0; i<car.length; i++) if(car[i]>='A' && car[i]<='Z') car[i] = (char)('a'+(car[i]-'A'));
         return new String(car);
     }
 }
