6d565664b0fbcb3acf250f727e475c7f34ab2374
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-03-30 23:26:02 -0500

    remove unnecessary constructor

--- src/main/java/org/example/sum_of_left_leaves/Solution.java
+++ src/main/java/org/example/sum_of_left_leaves/Solution.java
@@ -7,11 +7,6 @@ public class Solution {
       TreeNode right;
       TreeNode() {}
       TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
   }
     public int sumOfLeftLeaves(TreeNode root) {
         return dfs(root, false);
