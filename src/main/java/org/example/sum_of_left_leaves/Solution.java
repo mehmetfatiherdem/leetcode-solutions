71a048f9de98c2e2753fa3168f66dc6774de1260
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-04-03 00:11:23 -0500

    use treenode from data structure package

--- src/main/java/org/example/sum_of_left_leaves/Solution.java
+++ src/main/java/org/example/sum_of_left_leaves/Solution.java
@@ -1,14 +1,10 @@
 package org.example.sum_of_left_leaves;
 
+import org.example.data_structures.TreeNode;
+
 public class Solution {
-    class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-  }
     public int sumOfLeftLeaves(TreeNode root) {
         return dfs(root, false);
-
     }
     public int dfs(TreeNode root, boolean isLeft) {
         if(root == null) return 0;
