c2cafdcfbc693c8c62958916f388ab6d41cb6a1d
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-04-09 00:15:20 -0500

    remove mt

--- src/main/java/org/example/subtree_of_another_tree/Solution.java
+++ src/main/java/org/example/subtree_of_another_tree/Solution.java
@@ -7,7 +7,6 @@ public class Solution {
         TreeNode left;
     }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-
         if(subRoot == null) return true;
         if(root == null) return false;
         if(sameTree(root, subRoot)) return true;
