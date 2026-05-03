c83b8fb522480f1de2e7287cbfd54d177e893565
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2026-05-03 13:33:11 -0500

    remove mt

--- src/main/java/org/example/set_matrix_zeroes/Solution.java
+++ src/main/java/org/example/set_matrix_zeroes/Solution.java
@@ -26,9 +26,7 @@ public class Solution {
         }
 
         if(matrix[0][0] == 0) {
-            for(int i = 0; i<matrix.length; i++) {
-                matrix[i][0] = 0;
-            }
+            for(int i = 0; i<matrix.length; i++) matrix[i][0] = 0;
         }
         if(rowZero) Arrays.fill(matrix[0], 0);
     }
